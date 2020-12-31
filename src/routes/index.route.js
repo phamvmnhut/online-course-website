72b75fd26662ae822c01f4469b74fe74531f81d5
- <->
2020-12-31 21:02:21 +0700

    add see details button

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -17,24 +17,29 @@ router.get('/home', function(req, res) {
   });
 });
 
-router.get('/course-details', function (req, res) {
-  res.render('guest/course-details.hbs', {
+router.get('/course_details', function (req, res) {
+  res.render('guest/course_details.hbs', {
     layout: 'guest_layout'
   })
 })
 
 
-router.get('/course-list', function (req, res) {
-  res.render('guest/course-list.hbs', {
+router.get('/course_list', function (req, res) {
+  res.render('guest/course_list.hbs', {
     layout: 'guest_layout'
   })
 })
 
-router.get('/sign-up', function (req, res) {
-  res.render('guest/sign-up.hbs', {
+router.get('/sign_up', function (req, res) {
+  res.render('guest/sign_up.hbs', {
     layout: 'guest_layout'
   })
 })
 
+router.get('/search_results', function (req, res) {
+  res.render('guest/search_results.hbs', {
+    layout: 'guest_layout'
+  })
+})
 
 module.exports = router;
